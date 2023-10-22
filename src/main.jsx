1eb7c644cf4eaf29bb76c6fe20c14d6dcef7c753
afsal ms <afsalms715@gmail.com>
2023-10-22 22:16:14 +0300

    stash for react router dom for admin login and pannel

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './app/Store.js'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Switch>
+        <Route path='/' component={App}/>
+      </Switch>
+    </Router>
   </Provider>,
 )
