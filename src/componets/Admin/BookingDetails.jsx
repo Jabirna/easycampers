311c52af0679e1f57db7f6b9b61e9a6828f5dce9
afsal ms <afsalms715@gmail.com>
2023-11-09 22:56:13 +0300

    booking details read from db

--- src/componets/Admin/BookingDetails.jsx
+++ src/componets/Admin/BookingDetails.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { getDocs,collection } from 'firebase/firestore/lite'
+import { db } from '../firebaseConfig'
 
-const BookingDetails = () => {
+const BookingDetails =() => {
+    useEffect(()=>{
+        async function fetchData(){
+        const responce=await getDocs(collection(db,'booking'))
+        const bookingData=responce.docs.map(item=>item.data())
+        console.log(bookingData)
+        }
+        fetchData()
+    },[])
   return (
     <div>BookingDetails</div>
   )
