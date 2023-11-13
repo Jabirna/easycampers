f03f0f266288e894ac5d814fad3d3fd037a95a98
afsal ms <afsalms715@gmail.com>
2023-11-13 19:05:56 +0300

    prime react Datatable impliment

--- src/componets/Admin/BookingDetails.jsx
+++ src/componets/Admin/BookingDetails.jsx
@@ -1,18 +1,32 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import { getDocs,collection } from 'firebase/firestore/lite'
 import { db } from '../firebaseConfig'
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 const BookingDetails =() => {
+    const[BookingData,setBookingData]=useState([])
     useEffect(()=>{
         async function fetchData(){
         const responce=await getDocs(collection(db,'booking'))
         const bookingData=responce.docs.map(item=>item.data())
         console.log(bookingData)
+        setBookingData(bookingData)
         }
         fetchData()
     },[])
   return (
-    <div>BookingDetails</div>
+    <div className='p-5 rounded-sm'>
+      {BookingData.length!=0 &&
+      <DataTable value={BookingData} paginator rows={15} rowsPerPageOptions={[10, 15, 25, 50,100]} className='m-2 ring-[0.5px]' tableStyle={{ minWidth: '50rem' }}>
+        <Column field="name" sortable className='ring-[0.5px] p-1' header="Name"></Column>
+        <Column field="phone" sortable className='ring-[0.5px] text-center p-1' header="Phone"></Column>
+        <Column field="email" sortable className='ring-[0.5px] text-center p-1' header="Email"></Column>
+        <Column field="startDate" sortable className='ring-[0.5px] text-center p-1' header="Check IN"></Column>
+        <Column field="endDate" sortable className='ring-[0.5px] text-center p-1' header="Check Out"></Column>
+        <Column field="noOfPeople" sortable className='ring-[0.5px] text-center p-1' header="Number Of Gust"></Column>     
+    </DataTable>}
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import { store } from './app/Store.js'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import AdminPannel from './componets/Admin/AdminPannel.jsx'
+import { PrimeReactProvider } from 'primereact/api';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <Router>
-        <Routes>
-          <Route path='/' exact basename="/" Component={App}/>
-          <Route path='/admin' basename="/admin" Component={AdminPannel}/>
-        </Routes>
-    </Router>
-  </Provider>,
+  <PrimeReactProvider>
+    <Provider store={store}>
+      <Router>
+          <Routes>
+            <Route path='/' exact basename="/" Component={App}/>
+            <Route path='/admin' basename="/admin" Component={AdminPannel}/>
+          </Routes>
+      </Router>
+    </Provider>
+  </PrimeReactProvider>,
 )
